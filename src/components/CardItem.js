7e3cbcb1458e305a6da0d889d884d7d3c4cb63c3
adela miclea <adelamiclea709@yahoo.com>
2025-12-06 19:11:40 +0200

    addToCart

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -1,17 +1,39 @@
 import React from "react";
+import { moneyFormater } from "../helper";
 
 
 const CardItem=({
     product, money,total,basket,setBasket
 })=>{
-    const addItem=()=>{}
+    const addItem=()=>{
+        const checkBasket=basket.find((item)=>item.id===product.id)
+        if(checkBasket){
+            checkBasket.amount=checkBasket.amount+1;
+            setBasket(
+                [...basket.filter((item)=>item.id!==product.id),checkBasket]
+            )
+        }else{
+            setBasket([...basket,{
+                id:product.id,
+                amount:1,
+                title:product.title,
+                price:product.price
+            }
+            ])
+        }
+    }
     const removeItem=()=>{}
     return(
         <div className="cart_item">
             <img src={product.image} alt={product.title} />
             <div className="card_text">
                 <h1>{product.title}</h1>
-                <h5>{product.price}</h5>
+                <h5>{moneyFormater(product.price)}</h5>
+            </div>
+            <div className="actions">
+                <span>0</span>
+                <button className="btn btn_buy" onClick={addItem}>Add</button>
+                <button className="btn btn_sell" onClick={removeItem}>Interest</button>
             </div>
         </div>
     )
